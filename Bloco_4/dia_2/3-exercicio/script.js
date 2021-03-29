0f531e76bda3f6b229b48a2f266f29aa4c574d14
Alan Gonçalves <vekio.soft@gmail.com>
2021-03-29 09:52:53 -0300

    exercicio sobre estrut. repetição for - bloco 4.2

---
+++ Bloco_4/dia_2/3-exercicio/script.js
@@ -0,0 +1,23 @@
+// Exercicio 1
+// Acessar os valores do array utilizando o comando de repetição for:
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+// Acessar os índices do array:
+for(index = 0; index < cars.length; index += 1) {
+  console.log(index);
+}
+
+// Acessa os valores do array:
+for(index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
+
+// Exercicio 2
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let index = 0; index < groceryList.length; index += 1) {
+  console.log(groceryList[index]);
+}
