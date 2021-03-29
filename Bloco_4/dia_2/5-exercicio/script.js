1f746f260b1e0671aa10b1d7d24294bcdd189333
Alan Gonçalves <vekio.soft@gmail.com>
2021-03-29 14:54:08 -0300

    conclusão do exercicio 6 - bloco 4.2

---
+++ Bloco_4/dia_2/5-exercicio/script.js
@@ -0,0 +1,88 @@
+
+// Aprofunde seus conhecimentos
+// Leia atentamente os enunciados e faça o que se pede! Você irá utilizar esse array para realizar os exercícios do 1 ao 7:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercício 1
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log():
+
+// for (var index = 0; index < numbers.length; index += 1) {
+//   console.log(numbers[index]);
+// }
+
+// Exercício 2
+// Somar todos os valores contidos no array e imprimir o resultado:
+
+// let soma = 0;
+// for (var index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index]
+// }
+// console.log(soma)
+
+
+// Exercício 3
+// Calcule e imprima a média aritmética dos valores contidos no array
+// A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index];
+//   var media = soma / numbers.length
+// }
+//   console.log(media);
+
+
+// Exercício 4
+// Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index];
+//   var media = soma / numbers.length
+// }
+// if (media > 20) {
+//   console.log('valor maior que 20');
+// } else {
+//   console.log('valor menor ou igual a 20')
+// }
+
+//Exercicio 5
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+// let result = 0;
+// for(let index = 0; index < numbers.length; index += 1) {    //numbers.length = tamanho do array
+//   if(numbers[index] > result) {   //number[index] = posiçoes do array [0], [1], [2], ....
+//     result = numbers[index]
+//   }
+// }
+// console.log(result);
+
+// // Abaixo temo uma outra forma de obter o maior valor contido no array. Utilizando a função Math.max(...numbers)
+
+//   let result1 = 0;
+
+//   result1 = Math.max(...numbers);  // Math.max(...numbers) retorna o maior valor do array
+//   console.log(result1);
+
+
+//Exercicio 6
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+// let odd = 0;
+
+// for(let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 == 1 ) {  
+//     odd += 1;
+//   }
+// }
+//   if ( odd > 0) { 
+//     console.log(odd);
+//   } else {
+//     console.log("even number not found")
+// }
+
