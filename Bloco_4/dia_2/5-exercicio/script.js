95c3881e38f400c0d4d6f3ecb1562389b4d884cb
Alan Gonçalves <vekio.soft@gmail.com>
2021-03-30 09:18:56 -0300

    conclusão dos exercicios do bloco 4.2

--- Bloco_4/dia_2/5-exercicio/script.js
+++ Bloco_4/dia_2/5-exercicio/script.js
@@ -11,6 +11,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //   console.log(numbers[index]);
 // }
 
+
 // Exercício 2
 // Somar todos os valores contidos no array e imprimir o resultado:
 
@@ -49,8 +50,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //   console.log('valor menor ou igual a 20')
 // }
 
-//Exercicio 5
 
+//Exercicio 5
 // Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 
 // let result = 0;
@@ -69,8 +70,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //   console.log(result1);
 
 
-//Exercicio 6
-
+// Exercicio 6
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
 // let odd = 0;
@@ -86,3 +86,37 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     console.log("even number not found")
 // }
 
+
+// Exercicio 7
+//Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+// let atual = 0;
+// let smallestNumber = 0;
+// for (let index = 0; index < numbers.length; index += 1){
+//     atual = numbers[index];
+//     if (atual < numbers[index + 1]) {
+//         smallestNumber = atual;
+//     }
+// }   
+// console.log(smallestNumber);
+
+
+// Exercicio 8
+// Utilizando for, crie uma array que vá de 1 até 25 e imprima o resultado;
+
+//  var result = [];
+
+//  for(let index = 0; index < 25; index += 1) {
+//     result.push(index + 1)
+//  }
+// console.log(result)
+
+
+// Exercicio 9
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+// var result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
+// for (let index = 0; index < result.length; index ++) {
+//   console.log(result[index]/2);
+// }
