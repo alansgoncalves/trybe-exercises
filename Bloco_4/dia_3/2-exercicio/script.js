0ec6c5ecfc049f154487938dd67919cc461ea636
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-05 18:36:49 -0300

    conclusão dos exercícios de pirâmide bloco 4.3

--- Bloco_4/dia_3/2-exercicio/script.js
+++ Bloco_4/dia_3/2-exercicio/script.js
@@ -175,13 +175,24 @@
 //  *   *
 // *******
 
-// let n = 7;
-// let ast = '*';
-// let line = ' ';
 
-// for (let index = 4; index <= 7; index += 1) {
-//   console.log(line.repeat(n-index) + ast.repeat((2 * index) - n))
-  
+// let n = 7;
+// let middle = (n + 1) / 2;
+// let controlLeft = middle;
+// let controlRight = middle;
+// let symbol = '*';
+// for (let line = 1; line <= middle; line += 1) {
+//   let outputLine = '';
+//   for (let col = 1; col <= n; col += 1) {
+//     if (col == controlLeft || col == controlRight || line == middle) {
+//       outputLine += symbol;
+//     } else {
+//       outputLine += ' ';
+//     }
+//   }
+//   controlLeft -= 1;
+//   controlRight += 1;
+//   console.log(outputLine);
 // }
 
 
