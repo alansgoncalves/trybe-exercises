287940a1126c87f08113103d34356eab3b82c277
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-06 10:45:24 -0300

    conclusão dos exercicios sobre objetos e for in - bloco 4.4

--- Bloco_4/dia_4/5-exercicio/script.js
+++ Bloco_4/dia_4/5-exercicio/script.js
@@ -18,25 +18,24 @@
 
 // 2. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
 
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim',
-};
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
 
-// console.table(info);
+// info.recorrente = 'Sim';
+
+// console.log(info);
 
 // Resultado:
 
-// ┌────────────┬──────────────────────────────────────────────────────────────────┐
-// │  (index)   │                              Values                              │
-// ├────────────┼──────────────────────────────────────────────────────────────────┤
-// │ personagem │                           'Margarida'                            │
-// │   origem   │                          'Pato Donald'                           │
-// │    nota    │ 'Namorada do personagem principal nos quadrinhos do Pato Donald' │
-// │ recorrente │                              'Sim'                               │
-// └────────────┴──────────────────────────────────────────────────────────────────┘
+// {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim'
+// };
 
 
 
@@ -77,6 +76,13 @@ let info = {
 
 //5. Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
 
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
 let newInfo = {
   personagem: 'Tio Patinhas',
   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
@@ -84,12 +90,40 @@ let newInfo = {
   recorrente: 'Sim',
 };
 
-console.log(info.personagem + ' e ' + newInfo.personagem);
-console.log(info.origem + ' e ' + newInfo.origem);
-console.log(info.nota + ' e ' + newInfo.recorrente)
+// Duas maneiras de resolver:
+
+
+// Forma 1
+
+// console.log(info.personagem + ' e ' + newInfo.personagem);
+// console.log(info.origem + ' e ' + newInfo.origem);
+// console.log(info.nota + ' e ' + newInfo.recorrente)
 
 // Resultado:
 
 // Margarida e Tio Patinhas
 // Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
 // Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+
+
+
+// Forma 2
+
+// for (let key in info) {
+//   if (
+//     info[key] === info.recorrente &&
+//     info[key] === 'Sim' &&
+//     newInfo[key] === 'Sim'
+//   ) {
+//     console.log('Ambos recorrentes');
+//   } else {
+//     console.log(info[key] + ' e ' + newInfo[key]);
+//   }
+// }
+
+// Resultado:
+
+// Margarida e Tio Patinhas
+// Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+// Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+// Ambos recorrentes
