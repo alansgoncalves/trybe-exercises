9450c56d3751acd30614a4c77688156f1bdf8cc0
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-06 23:26:08 -0300

    exercicios criando elementos parte 2 - bloco 5.2

---
+++ Bloco_5/dia_2/2-exercicio/script.js
@@ -0,0 +1,32 @@
+// 1. Crie um irmão para elementoOndeVoceEsta :
+
+// let novoElemento = document.createElement('div');
+// pai.appendChild(novoElemento.cloneNode());
+
+// console.log(novoElemento);
+
+
+
+// 2. Crie um filho para elementoOndeVoceEsta :
+
+// let novoElemento = document.createElement('div')
+// elementoOndeVoceEsta.appendChild(novoElemento.cloneNode());
+
+// console.log(novoElemento);
+
+
+
+// 3. Crie um filho para primeiroFilhoDoFilho :
+
+// let novoElemento = document.createElement('div')
+// primeiroFilho.appendChild(novoElemento.cloneNode());
+
+// console.log(novoElemento);
+
+
+// 4. A partir desse filho criado, acesse terceiroFilho :
+
+// let filhoDoPrimeiroFilho = primeiroFilho.firstElementChild;
+// let terceiroFilhoDoFilhoDoPrimeiroFilho = filhoDoPrimeiroFilho.parentElement.nextElementSibling.nextElementSibling;
+
+// console.log(filhoDoPrimeiroFilho);
