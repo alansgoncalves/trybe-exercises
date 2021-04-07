dee80ac5cd58e88aeff46ffcc859e0a53c0bee75
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-07 00:17:32 -0300

    exercicio final Funções e Manipulação - Bloco 5.1

---
+++ Bloco_5/dia_1/2-exercicio/script.js
@@ -0,0 +1,28 @@
+        /*
+        Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+        - document.getElementById()
+        - document.getElementsByClassName()
+        - document.getElementsByTagName()
+ Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+ Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+ Crie uma função que mude a cor do quadrado vermelho para branco.
+ Crie uma função que corrija o texto da tag <h1>.
+ Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+ Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+        */
+
+ document.querySelector('.center-content p:nth-child(2)').innerText =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+document.getElementsByClassName('main-content')[0].style.backgroundColor =
+  'rgb(76,164,109)';
+
+document.getElementsByClassName('center-content')[0].style.backgroundColor =
+  'white';
+
+document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - JavaScript';
+
+document.querySelectorAll('p').forEach(element => {
+  element.style.textTransform = 'uppercase';
+  console.log(element.innerText);
+});
