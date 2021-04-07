52ef512ef598c6a24ff896bb19de98d0739da55f
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-06 22:19:49 -0300

    exercicios buscando elementos parte 1 - bloco 5.2

---
+++ Bloco_5/dia_2/1-exercicio/script.js
@@ -0,0 +1,47 @@
+
+// 1. Acesse o elemento elementoOndeVoceEsta:
+// const elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+// console.log(elementoOndeVoceEsta);
+
+
+
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele:
+// const elementoPai = elementoOndeVoceEsta.parentElement;
+// elementoPai.style.color = 'green';
+// console.log(elementoPai);
+
+
+
+// 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+// const elementoOndeEstou = elementoOndeVoceEsta.firstElementChild.innerText = "Hoje acordei feliz";
+// console.log(elementoOndeEstou);
+
+
+
+// 4. Acesse o primeiroFilho a partir de pai:
+// const textH2 = pai.firstElementChild;
+// console.log(textH2);
+
+
+
+// 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta:
+// const primeiroFilho = elementoOndeVoceEsta.previousElementSibling;
+// console.log(primeiroFilho);
+
+
+
+// 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+// const textoAtencao = elementoOndeVoceEsta.nextSibling;
+// console.log(textoAtencao);
+
+
+
+// 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+// const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+// console.log(terceiroFilho);
+
+
+
+// 8. Agora acesse o terceiroFilho a partir de pai .
+// const terceiroFilhoDoPai = pai.lastElementChild.previousElementSibling;
+// console.log(terceiroFilhoDoPai);
