29280113155547c6c38a1a3013978205789c56b9
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-21 12:15:09 -0300

    exercicio de fixação 1 DOM e Seletores - bloco 5.1

--- Bloco_5/dia_1/exercicios/script.js
+++ Bloco_5/dia_1/exercicios/script.js
@@ -3,11 +3,11 @@
 // document.getElementById("paragraph");   // para alterar a cor do id=paragraph
 // paragraph.style.color = "red";
 
-// document.getElementById("page-title").innerText = "Filme favorito: Scarface";  // para altera o texto do id=page-title
+document.getElementById("page-title").innerText = "Filme favorito: Scarface";  // para altera o texto do id=page-title
 
-// document.getElementById("second-paragraph").innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+// document.getElementById("second-paragraph").innerText = "Lorem ipsum dolor sit";
 
-// document.getElementById("subtitle").innerText = "Tony Montana";
+document.getElementById("subtitle").innerText = "Tony Montana";
 
 
 // Parte 2
@@ -16,18 +16,18 @@
 // document.getElementsByClassName("text")[0].innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
 
 
-// para alterar todos os textos de uma só vez, temos que armazenar o getElementsByClassName dentro de uma variável e fazer um for loop:
-let paragrafos = document.getElementsByClassName("text");
+// // para alterar todos os textos de uma só vez, temos que armazenar o getElementsByClassName dentro de uma variável e fazer um for loop:
+let paragrafos = document.getElementsByTagName('p');
 
 for (let index = 0; index < paragrafos.length; index += 1) {
   paragrafos[index].innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
 }
 
-// Para alterar a cor de fundo de cada paragrafo:
-document.getElementsByClassName('text')[1].style.backgroundColor = "blue";
-document.getElementsByClassName('text')[0].style.backgroundColor = "pink";
+// // Para alterar a cor de fundo de cada paragrafo:
+document.getElementById('paragraph').style.backgroundColor = "blue";
+document.getElementById('second-paragraph').style.backgroundColor = "pink";
 
 
 // recuperar subtitulo pela tag:
-document.getElementsByTagName('h4')[0].innerText = "Tony Montana";
+// document.getElementsByTagName('h4')[0].innerText = "Subtítulo";
 
