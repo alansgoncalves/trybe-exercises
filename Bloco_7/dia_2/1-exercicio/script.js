0ef1ac8c452564c09352da08ae25d6c581867451
Alan Gonçalves <vekio.soft@gmail.com>
2021-05-23 21:26:10 -0300

    exercicio de fixaçao Objects - bloco 7.2

--- Bloco_7/dia_2/1-exercicio/script.js
+++ Bloco_7/dia_2/1-exercicio/script.js
@@ -84,6 +84,8 @@ console.log(customer);
 // }
 
 
+
+
 // Parte II - Object.keys
 
 const student1 = {
@@ -130,5 +132,144 @@ listSkills(student2);
 // Git, Nível: Bom
 
 
+
+
 // Parte III - Object.values
 
+const student = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkill: 'Ótimo',
+};
+
+const listSkillsValuesWithFor = (student) => {
+  const skills = [];
+  for(skill in student) {
+    skills.push(student[skill]);
+  }
+
+  return skills;
+};
+
+const listSkillsValuesWithValues = (student) => Object.values(student);
+
+// Sem Object.values
+console.log(listSkillsValuesWithFor(student));
+
+// Com Object.values
+console.log(listSkillsValuesWithValues(student));
+
+
+
+// Parte IV - Object.entries
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
+
+// RESULTADO:
+
+// [
+//   [ 'França', 'Paris' ],
+//   [ 'Brasil', 'Brasília' ],
+//   [ 'Espanha', 'Madrid' ],
+//   [ 'Portugal', 'Lisboa' ]
+// ]
+// --------
+// País: França
+// Capital: Paris
+// --------
+// País: Brasil
+// Capital: Brasília
+// --------
+// País: Espanha
+// Capital: Madrid
+// --------
+// País: Portugal
+// Capital: Lisboa
+
+
+
+
+// Parte V - Object.assign
+
+// A função recebe um número qualquer de parâmetros. Todos são agregados como valores para adicionar ao objeto de destino!
+Object.assign(destino, objeto1);
+Object.assign(destino, objeto1, objeto2);
+Object.assign(destino, objeto1, objeto2, objeto3, objeto4);
+
+
+// Exemplo:
+const person = {
+  name: 'Alberto',
+  lastName: 'Gomes',
+  age: 20,
+};
+
+const info = {
+  age: 23,
+  job: 'engenheiro',
+};
+
+const family = {
+  children: ['Maria', 'João'],
+  wife: 'Ana',
+};
+
+Object.assign(person, info, family)
+console.log(person)
+
+/* REUSLTADO:
+  { name: 'Alberto',
+  lastName: 'Gomes',
+  age: 23,
+  job: 'engenheiro',
+  children: [ 'Maria', 'João' ],
+  wife: 'Ana'
+  } */
+
+
+  // Exemplo 2
+
+  const person = {
+    name: 'Roberto',
+  };
+  
+  const lastName = {
+    lastName: 'Silva',
+  };
+  
+  const clone = Object.assign(person, lastName);
+  
+  console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+  console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+
+  // Exemplo 3
+
+  clone.name = 'Maria';
+
+console.log('Mudando a propriedade name através do objeto clone')
+console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log('--------------');
+
+person.lastName = 'Ferreira';
+
+console.log('Mudando a propriedade lastName através do objeto person');
+console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
