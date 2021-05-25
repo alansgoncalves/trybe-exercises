9b90c8a17365922d87b3ce5bb7614afeb3876857
Alan Gonçalves <vekio.soft@gmail.com>
2021-05-25 12:13:28 -0300

    exercicio de fixação Introdução React - Bloco 11.1

--- modulo-2/Bloco_11/dia_1/2-exercicio/11-1-exercice/src/App.js
+++ modulo-2/Bloco_11/dia_1/2-exercicio/11-1-exercice/src/App.js
@@ -1,6 +1,11 @@
 
 import './App.css';
 
+//1. Crie um novo projeto utilizando npx create-react-app nome-app e acesse a pasta nome-app
+//2. Crie uma lista de tarefas simples seguindo os passos abaixo
+//3. agora, chame a função dentro do seu componente App (não se esqueça da sintaxe JSX!). Insira o valor que você quiser, salve a página e inicie-a rodando o comando npm start .
+//4. Por fim, crie uma array de compromissos e use a função map para que cada item do array apareça, como um item de lista, no seu componente App .
+
 const Task = (value) => {
   return (
     <li key={value}>{value}</li>
