7767af0497bb72f3001c8b9851e312e5b1ef175d
Alan Gonçalves <vekio.soft@gmail.com>
2021-05-30 19:43:58 -0300

    exercicio de React - My Pokedex - bloco 11.2

--- modulo-2/Bloco_11/dia_2/2-exercicio/my-pokedex/src/index.js
+++ modulo-2/Bloco_11/dia_2/2-exercicio/my-pokedex/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Pokedex2 from './Pokedex';
 
 ReactDOM.render(
   <React.StrictMode>
