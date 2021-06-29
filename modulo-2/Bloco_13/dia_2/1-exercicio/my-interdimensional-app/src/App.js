366eca00ea3ca81632b52e2a56ff2e3e409dcf14
Alan Gonçalves <vekio.soft@gmail.com>
2021-06-29 11:33:25 -0300

    exercicio de fixação React Router e componente Link - bloco 13.2

---
+++ modulo-2/Bloco_13/dia_2/1-exercicio/my-interdimensional-app/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users">Users</Link></li>
+        </ul>
+      <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/users" render={(props) => <Users {...props} greetingsMessage="Good morning" />}></Route>
+      </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
