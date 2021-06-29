4b4bdb3c7704f0614115c33a1f5302b4bfd82026
Alan Gonçalves <vekio.soft@gmail.com>
2021-06-29 12:16:10 -0300

    conclusão requisitos do 1 ao 13 - bloco 13.2

--- modulo-2/Bloco_13/dia_2/1-exercicio/my-interdimensional-app/src/App.js
+++ modulo-2/Bloco_13/dia_2/1-exercicio/my-interdimensional-app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
+import StrictAccess from './components/StrictAccess';
+
 
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
@@ -13,12 +15,14 @@ class App extends Component {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/users">Users</Link></li>
+          <li><Link to="/strict-access">Strict Access</Link></li>
         </ul>
-      <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/users" render={(props) => <Users {...props} greetingsMessage="Good morning" />}></Route>
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good morning" />}></Route>
+          <Route path="/strict-access"><StrictAccess user={{username: 'joao', password: '1234'}} /></Route>
+        </Switch>
       </BrowserRouter>
     );
   }
