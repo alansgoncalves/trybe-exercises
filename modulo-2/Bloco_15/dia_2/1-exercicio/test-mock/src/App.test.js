eccdde990920f725b0da28f9ea60535019bb8ce4
Alan Gonçalves <vekio.soft@gmail.com>
2021-07-04 10:45:59 -0300

    exercicio sobre Mocks e Inputs - bloco 15.2

---
+++ modulo-2/Bloco_15/dia_2/1-exercicio/test-mock/src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('Verifica que quando recebo uma piada, mostro ela na tela', () => {
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200
+  }
+});
