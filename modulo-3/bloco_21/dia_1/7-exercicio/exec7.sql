9a2bba46f3b34cc5064e6de1a848d40047be84af
Alan Gonçalves <vekio.soft@gmail.com>
2021-08-28 15:01:11 -0300

    exercicio praticando GROUP BY - bloco 21.1

---
+++ modulo-3/bloco_21/dia_1/7-exercicio/exec7.sql
@@ -0,0 +1,27 @@
+USE sakila;
+
+SELECT * FROM customer;
+SELECT * FROM film;
+SELECT * FROM address;
+
+-- 1. Monte uma query que exiba a quantidade de clientes cadastrados na tabela sakila.customer
+--  que estão ativos e a quantidade que estão inativos:
+SELECT active, COUNT(*) FROM sakila.customer GROUP BY active;
+
+-- 2. Monte uma query para a tabela sakila.customer que exiba a quantidade de clientes ativos e inativos por loja.
+-- Os resultados devem conter o ID da loja , o status dos clientes (ativos ou inativos) e a quantidade de clientes por status:
+SELECT store_id, active, COUNT(*) FROM sakila.customer GROUP BY store_id, active;
+
+-- 3. Monte uma query que exiba a média de duração de locação por classificação indicativa ( rating ) dos filmes cadastrados
+-- na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor.
+SELECT rating, AVG(rental_duration) AS avg_rental FROM sakila.film GROUP BY rating ORDER BY avg_rental DESC;
+-- OU
+SELECT AVG(rental_duration) AS avg_rental_duration, rating FROM sakila.film GROUP BY rating ORDER BY avg_rental_duration DESC;
+
+-- 4. Monte uma query para a tabela sakila.address que exiba o nome do distrito e a quantidade de endereços registrados nele. 
+-- Os resultados devem ser ordenados da maior quantidade para a menor.
+SELECT district, COUNT(*) FROM sakila.address GROUP BY district ORDER BY COUNT(*) DESC;
+
+
+
+
