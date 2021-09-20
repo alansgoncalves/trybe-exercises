f04dddf0ae0d250b1f04197863ff467c70af6ccc
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-20 18:44:38 -0300

    exercicio utilizando operador  - bloco 25.1

---
+++ modulo-3/bloco_25/dia_1/2-exercicio/exec2.js
@@ -0,0 +1,53 @@
+use('agg_example');
+
+db.transactions.find();
+
+// 1. Selecione todos os bancos, ou seja, valores do campo bank:
+db.transactions.aggregate([
+  {
+    $group: {
+      _id: '$bank',
+      bank: { $sum: 1 }
+    }
+  }
+]);
+
+// 2. Selecione o valor total das transações em cada banco e quantas são:
+db.transactions.aggregate(
+  [
+    {
+      $group: {
+        _id: "$bank",
+        totalTransactionsAmount: { $sum: "$value" },
+        count: { $sum: 1}
+      }
+    }
+  ]
+);
+
+// 3. Selecione o valor total de transações por banco:
+db.transactions.aggregate(
+  [
+    {
+      $group: {
+        _id: "$bank",
+        totalTransactions: { $sum: "$value" },
+      }
+    }
+  ]
+);
+
+// 4. Selecione os bancos que têm o valor total de transações maior que 1000:
+db.transactions.aggregate(
+  [
+    {
+      $group: {
+        _id: "$bank",
+        totalTransactions: { $sum: "$value" },
+      }
+    },
+    {
+      $match: { "totalTransactions": { $gt: 1000 } }
+    }
+  ]
+);
