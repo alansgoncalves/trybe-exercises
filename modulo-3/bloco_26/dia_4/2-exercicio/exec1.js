0c218b60120ac1ebfa930a1ee54edee2433df4dc
Alan Gonçalves <vekio.soft@gmail.com>
2021-10-08 16:22:11 -0300

    add exec 1

---
+++ modulo-3/bloco_26/dia_4/2-exercicio/exec1.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+
+// app.get('/ping', function (req, res) {
+//   res.send({ message: 'pong' });
+// });
+
+// OU
+
+app.get('/ping', (_req, res) => res.json({ "message": "pong" }));
+
+
+app.use(function (err, req, res, next) {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
+});
+
+app.listen(3000, () => console.log('ouvindo na porta 3000!'));
