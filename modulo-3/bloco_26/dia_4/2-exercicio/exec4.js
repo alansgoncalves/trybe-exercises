c9d0ea29d2314b470c930459d26ec9e53c153b90
Alan Gonçalves <vekio.soft@gmail.com>
2021-10-09 10:07:33 -0300

    add exec 4 - bloco 26.4

---
+++ modulo-3/bloco_26/dia_4/2-exercicio/exec4.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+const app = express();
+// app.use(bodyParser.json());
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.body;
+  res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` })
+})
+
+app.use(function (err, req, res, next) {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
+});
+
+app.listen(3000, () => console.log('ouvindo na porta 3000!'));
