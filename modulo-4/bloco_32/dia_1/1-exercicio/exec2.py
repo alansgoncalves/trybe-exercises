0554f95bd5e10a7b4426e40f5a0e3d268a32031a
Alan Gonçalves <vekio.soft@gmail.com>
2022-02-13 16:06:43 -0300

    exercicio pratico em Python - bloco 32.1

---
+++ modulo-4/bloco_32/dia_1/1-exercicio/exec2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+
+print(minutes)
+print(seconds)
+
+
---
+++ modulo-4/bloco_32/dia_1/1-exercicio/exec4.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+books = 60
+book_price = (1 - 0.4) * 24.20
+logistic = 3 + (books - 1) * 0.75
+
+cost = books * book_price + logistic
+
+print(cost)
