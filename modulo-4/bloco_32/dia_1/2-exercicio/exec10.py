07cce4c69b71f3857c244f13f865f4e8bdfcad18
Alan Gonçalves <vekio.soft@gmail.com>
2022-02-21 08:40:50 -0300

    exercicio pratico em Python - bloco 32.1

--- modulo-4/bloco_32/dia_1/2-exercicio/exec10.py
+++ modulo-4/bloco_32/dia_1/2-exercicio/exec10.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
-
-my_list = [20, 20, 1, 2];
-count_elements = {
-    20: 2,
-    1: 1,
-    2: 2
+info = {
+  "personagem": "Margarida",
+  "origem": "Pato Donald",
+  "nota": "Namorada do personagem principal nos quadrinhos do Pato Donald",
 }
+
+del info["origem"]
+
+
+# vamos converter o range em uma lista para ajudar na visualização
+
+# definimos somente o valor de parada
+list(range(5))  # saída: [0, 1, 2, 3, 4]
+
+# definimos o valor inicial e o de parada
+list(range(1, 6))  # saída: [1, 2, 3, 4, 5]
+
+# definimos valor inicial, de parada e modificamos o passo para 2
+list(range(1, 11, 2))  # saída: [1, 3, ,5 ,7 , 9]
+
+# podemos utilizar valores negativos para as entradas também
+list(range(10, 0, -1))  # saída: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
